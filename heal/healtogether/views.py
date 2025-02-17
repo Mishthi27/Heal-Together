b778ec20f286c6e3988519254c944e422af5c46b
Mishthi27 <mishthisachdeva27@gmail.com>
2025-02-17 17:13:42 +0530

    updated static folder

--- heal/healtogether/views.py
+++ heal/healtogether/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here. 
 
 def index(request): 
-    return render(request, 'index.html') 
+    return render(request, 'HealTogether/index.html') 
 
 def login(request): 
     return render(request, 'login.html') 
